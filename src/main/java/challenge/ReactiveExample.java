75c4da89cf315e89f40726d86d6fc01232ca0c5a
Kevin Vargas <kf11.vargas@gmail.com>
2022-07-12 07:06:32 -0500

    Se crean los metodos reactivos.

--- src/main/java/challenge/ReactiveExample.java
+++ src/main/java/challenge/ReactiveExample.java
@@ -4,7 +4,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 public class ReactiveExample {
@@ -15,19 +19,21 @@ public class ReactiveExample {
     public ReactiveExample() {
         //TODO: convertir los estudiantes a un Flux
 
-       /* estudianteList = List.of(
+       estudianteList = Flux.fromIterable(List.of(
                 new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
                 new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
                 new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
                 new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
                 new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
-        );
-*/
+        ));
+
     }
 
     //TODO: suma de puntajes
     public Mono<Integer> sumaDePuntajes() {
-        return null;
+        return estudianteList
+                .map(mapeoDeEstudianteAPuntaje())
+                .reduce(Integer::sum);
     }
 
     private Function<Estudiante, Integer> mapeoDeEstudianteAPuntaje() {
@@ -36,37 +42,56 @@ public class ReactiveExample {
 
     //TODO: mayor puntaje de estudiante
     public Mono<Estudiante> mayorPuntajeDeEstudiante(int limit) {
-        return null;
-
+        return estudianteList
+                .sort(Comparator.comparing(Estudiante::getPuntaje).reversed())
+                .next();
     }
 
     //TODO: total de asisntencias de estudiantes con mayor puntaje basado en un  valor
     public Mono<Integer> totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(int valor) {
-        return null;
+        return estudianteList
+                .filter(estudiante -> estudiante.getPuntaje()>70)
+                .map(estudiante -> estudiante.getAsistencias())
+                .flatMap(integers -> Flux.fromIterable(integers))
+                .reduce(Integer::sum);
     }
 
     //TODO: el estudiante tiene asistencias correctas
     public Mono<Boolean> elEstudianteTieneAsistenciasCorrectas(Estudiante estudiante) {
-        return null;
+        return estudianteList
+                .filter(estudianteList -> estudianteList.getNombre().equals(estudiante.getNombre()))
+                .next()
+                .zipWith(Mono.just(estudiante), (est1, est2) -> est1.getAsistencias().equals(est2.getAsistencias()));
     }
 
     //TODO: promedio de puntajes por estudiantes
     public Mono<Double> promedioDePuntajesPorEstudiantes() {
-        return null;
+        return estudianteList
+                .collect(Collectors.averagingDouble(Estudiante::getPuntaje));
     }
 
 
     //TODO: los nombres de estudiante con puntaje mayor a un valor
     public Flux<String> losNombresDeEstudianteConPuntajeMayorA(int valor) {
-        return null;
+        return estudianteList
+                .filter(estudiante -> estudiante.getPuntaje()>valor)
+                .map(Estudiante::getNombre);
     }
-
-
 
     //TODO: estudiantes aprovados
     public Flux<String> estudiantesAprovados(){
-        return null;
+        return estudianteList
+                .map(this::aprobar)
+                .filter(Estudiante::isAprobado)
+                .map(Estudiante::getNombre);
     }
 
+    public Estudiante aprobar(Estudiante estudiante){
+        var est = new Estudiante(estudiante.getNombre(),estudiante.getPuntaje(),estudiante.getAsistencias());
+        if(estudiante.getPuntaje()>=75){
+            est.setAprobado(true);
+        }
+        return est;
+    }
 
 }
